f7d3e91a8f8e79165f4d56853ccc2a05478746b2
Muhammad Waris <bhattimuhammadwaris@gmail.com>
2026-02-05 16:52:18 +0500

    Added another event
    Added a new event called handleSubmit and wrapped inputs in form tag

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,14 @@ function App() {
     // setName(e.target.value)
     setForm({...form , [e.target.name]:e.target.value})
   }
+
+    // NEW EVENT
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log("Form Submitted")
+    console.log(form)
+  }
+
   return (
     <>
        <div className="app">
@@ -25,12 +33,14 @@ function App() {
     <div className="button">
       <button onClick={handleclick}>Click me</button>
     </div>
-
+      <form onSubmit={handleSubmit}>
     <label>Email:</label>
     <input type="text" name="email" value={form.email} onChange={handlechange}/>
 
     <label>Phone No:</label>
     <input type="number" name="phone" value={form.phone} onChange={handlechange}/>
+        <button type="submit">Submit</button>
+        </form>
   </div>
     </>
   )
